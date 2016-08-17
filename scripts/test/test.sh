b32d61b103c3c18e1c82da973dccd8a63d4dc1f8
Hyunji Kim <Hyunji.Kim@ca.com>
2016-08-16 23:08:26 -0700

    test.sh

--- scripts/test/test.sh
+++ scripts/test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
-docker rm -f landingpage-test
-docker run --name landingpage-test -i -e SAUCE_USERNAME=$SAUCE_USERNAME -e SAUCE_ACCESS_KEY=$SAUCE_ACCESS_KEY landingpage-build ./scripts/test/test-app.sh
+docker rm -f landingpage-test-client
+docker run --name landingpage-test-client -i -e SAUCE_USERNAME=$SAUCE_USERNAME -e SAUCE_ACCESS_KEY=$SAUCE_ACCESS_KEY landingpage-build ./scripts/test/test-app.sh
