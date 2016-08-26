822d62dc4494a612bcab55ac99734b02a034ce13
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-08-25 23:07:38 -0700

    Update bake.sh

--- scripts/bake/bake.sh
+++ scripts/bake/bake.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
-filename=`docker run landingpage-build ls target`
-docker run landingpage-build cat target/$filename | tee target/$filename > /dev/null
 docker build -t gcr.io/qubeship/landingpage -f scripts/bake/Dockerfile .
