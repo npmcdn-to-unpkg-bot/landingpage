8f40ce8db7aa5b01b83b1aef8aa56c7e9ffa499d
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-08-25 23:05:12 -0700

    Update build.sh

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -2,3 +2,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../..
 docker build -t landingpage-build -f scripts/build/Dockerfile .
+filename=`docker run landingpage-build ls target`
+docker run landingpage-build cat target/$filename | tee target/$filename > /dev/null
